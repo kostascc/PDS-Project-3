9e82cbd21e84c551b9f01290f864e5a2d07c0fe7
Kostas <kachatzis@ece.auth.gr>
2021-02-25 16:48:51 +0200

    Add GPU files (no implementation)

---
+++ NLM/GPU.cu
@@ -0,0 +1,46 @@
+﻿#include "GPU.cuh"
+
+using namespace std;
+
+using std::cout;
+using std::generate;
+using std::vector;
+
+// Threads per CTA dimension
+constexpr int _THREADS = 1 << 5;
+
+// Size of shared memory per Thread Block
+constexpr int _SHMEM_SIZE = _THREADS * _THREADS;
+
+
+
+
+int main2(int argc, char** argv)
+{
+
+	int _n = 0;
+	int _threads = _THREADS;
+	int _blocks = (_n + _threads - 1) / _threads;
+
+	dim3 _dim_blocks(_blocks, _blocks);
+	dim3 _dim_threads(_threads, _threads);
+
+	krnl <<<_dim_blocks, _dim_threads >>> ();
+	cudaDeviceSynchronize();
+
+
+
+	return EXIT_SUCCESS;
+
+}
+
+
+
+__global__ void krnl()
+{
+	int row = threadIdx.x * blockDim.x + threadIdx.x;
+	int col = threadIdx.y * blockDim.y + threadIdx.y;
+
+
+	return;
+}
---
+++ NLM/GPU.cuh
@@ -0,0 +1,39 @@
+﻿#ifndef GPU_CUH
+#define GPU_CUH
+
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+#include "Utils.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "math.h"
+#include <omp.h>
+
+
+#define USE_LOG_FILE
+
+
+using namespace std;
+using namespace parameters;
+
+namespace GPU
+{
+
+	int run(Parameters params);
+
+}
+
+
+#endif
