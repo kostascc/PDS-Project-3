5864b1382b42fba8b2fa8ed85453402dec660de3
Kostas <kachatzis@ece.auth.gr>
2021-02-25 16:46:21 +0200

    Fix Image Import
    Support only 8-bit grayscale bitmaps, set option for byte normalization.

--- NLM/Utils.cpp
+++ NLM/Utils.cpp
@@ -37,11 +37,12 @@ utils::ImageFile::ImageFile()
 
 
 /**
- * Written by Elejandro Rodriguez
+ * Edited from Elejandro Rodriguez's answer on StackOverFlow
  **/
 void utils::ImageFile::Read(string filePath)
 {
 
+	byte* tmpPixelArr;
 	//Open the file for reading in binary mode
 	FILE* imageFile = fopen(filePath.c_str(), "rb");
 	//Read data offset
@@ -61,7 +62,21 @@ void utils::ImageFile::Read(string filePath)
 	//Allocate a pixel array
 	bytesPerPixel = ((int32)bitsPerPixel) / 8;
 
-	cout << "Read: " << height << "x" << width << ", bytesPerPixel:" << bytesPerPixel << "\n";
+#ifdef ONLY_8_BIT_BMP
+	if (bitsPerPixel != 8)
+	{
+		cout << "Bitmap was in wrong format (8-bit allowed only)\n";
+		exit(1);
+	}
+#endif
+
+#ifdef ONLY_RECTANGULAR
+	if (height != width)
+	{
+		cout << "Only Rectangular Pictures Allowed!\n";
+		exit(1);
+	}
+#endif
 
 	//Rows are stored bottom-up
 	//Each row is padded to be a multiple of 4 bytes. 
@@ -72,12 +87,13 @@ void utils::ImageFile::Read(string filePath)
 	int unpaddedRowSize = (width) * (bytesPerPixel);
 	//Total size of the pixel data in bytes
 	int totalSize = unpaddedRowSize * (height);
-	pixelArr = (byte*)malloc(totalSize);
+	mSize = totalSize;
+	tmpPixelArr = (byte*)malloc(totalSize);
 	//Read the pixel data Row by Row.
 	//Data is padded and stored bottom-up
 	int i = 0;
 	//point to the last row of our pixel array (unpadded)
-	byte* currentRowPointer = pixelArr + ((height - 1) * unpaddedRowSize);
+	byte* currentRowPointer = tmpPixelArr + ((height - 1) * unpaddedRowSize);
 	for (i = 0; i < height; i++)
 	{
 		//put file cursor in the next row from top to bottom
@@ -90,16 +106,83 @@ void utils::ImageFile::Read(string filePath)
 
 	fclose(imageFile);
 
+	// Copy Byte array into float array
+	pixelArr = (float*)malloc(height * width * sizeof(float));
+
+	for (int i = 0; i < height * width; i++)
+	{
+
+		/*byte a[4] = {
+			tmpPixelArr[i * bytesPerPixel + 3] & 0xFF,
+			tmpPixelArr[i * bytesPerPixel + 2] & 0xFF,
+			tmpPixelArr[i * bytesPerPixel + 1] & 0xFF,
+			tmpPixelArr[i * bytesPerPixel + 0] & 0xFF };
+		*/
+
+		//memcpy(pixelArr[i], a, 4);
+
+		pixelArr[i] = ((float)(tmpPixelArr[i] & 0xFF));
+#ifdef NORMALIZE_PIXELS
+		pixelArr[i] /= 255.0f;
+#endif
+
+	}
+
+
 	return;
 }
 
 
 
 /**
- * Written by Elejandro Rodriguez
+ * Edited from Elejandro Rodriguez's answer on StackOverFlow
  **/
 void utils::ImageFile::Write(string filePath)
 {
+
+	byte* tmpPixelArr = (byte*)malloc(height * width * 4 * sizeof(byte));
+	int l;
+	for (int i = 0; i < height * width; i++)
+	{
+#ifdef NORMALIZE_PIXELS
+		pixelArr[i] *= 255.0f;
+#endif
+
+		if (pixelArr[i] > 255.0f)
+		{
+			cout << "pixel greater than 255.0f found!\n";
+			cout << "(" << pixelArr[i] << ")\n";
+#ifdef FIX_PIXELS_OUT_OF_BOUND
+			pixelArr[i] = 255.0f;
+#else
+			exit(1);
+#endif
+		}
+
+		if (pixelArr[i] < 0.0f)
+		{
+			cout << "pixel lower than 0.0f found!\n";
+			cout << "(" << pixelArr[i] << ")\n";
+#ifdef FIX_PIXELS_OUT_OF_BOUND
+			pixelArr[i] = 0.0f;
+#else
+			exit(1);
+#endif
+		}
+
+		byte b = ((byte)(int)(pixelArr[i]) & 0xFF);
+
+		tmpPixelArr[i * 4 + 0] = b;
+		tmpPixelArr[i * 4 + 1] = b;
+		tmpPixelArr[i * 4 + 2] = b;
+		tmpPixelArr[i * 4 + 3] = b;
+
+#ifdef NORMALIZE
+		pixelArr[i] /= 255.0f;
+#endif
+	}
+
+
 	//Open file in binary mode
 	FILE* outputFile = fopen(filePath.c_str(), "wb");
 	//*****HEADER************//
@@ -108,7 +191,7 @@ void utils::ImageFile::Write(string filePath)
 	fwrite(&BM[0], 1, 1, outputFile);
 	fwrite(&BM[1], 1, 1, outputFile);
 	//Write file size considering padded bytes
-	int paddedRowSize = (int)(4 * ceil((float)width / 4.0f)) * bytesPerPixel;
+	int paddedRowSize = (int)(4 * ceil((float)width / 4.0f)) * bytesPerPixel * 4;
 	int32 fileSize = paddedRowSize * height + HEADER_SIZE + INFO_HEADER_SIZE;
 	fwrite(&fileSize, 4, 1, outputFile);
 	//Write reserved
@@ -129,13 +212,13 @@ void utils::ImageFile::Write(string filePath)
 	int16 planes = 1; //always 1
 	fwrite(&planes, 2, 1, outputFile);
 	//write bits per pixel
-	int16 bitsPerPixel = bytesPerPixel * 8;
+	int16 bitsPerPixel = bytesPerPixel * 8 * 4;
 	fwrite(&bitsPerPixel, 2, 1, outputFile);
 	//write compression
 	int32 compression = NO_COMPRESION;
 	fwrite(&compression, 4, 1, outputFile);
 	//write image size (in bytes)
-	int32 imageSize = width * height * bytesPerPixel;
+	int32 imageSize = width * height * bytesPerPixel * 4;
 	fwrite(&imageSize, 4, 1, outputFile);
 	//write resolution (in pixels per meter)
 	int32 resolutionX = 11811; //300 dpi
@@ -148,16 +231,54 @@ void utils::ImageFile::Write(string filePath)
 	//Write important colors
 	int32 importantColors = ALL_COLORS_REQUIRED;
 	fwrite(&importantColors, 4, 1, outputFile);
+
 	//write data
 	int i = 0;
-	int unpaddedRowSize = width * bytesPerPixel;
+	int unpaddedRowSize = width * bytesPerPixel * 4;
 	for (i = 0; i < height; i++)
 	{
 		//start writing from the beginning of last row in the pixel array
 		int pixelOffset = ((height - i) - 1) * unpaddedRowSize;
-		fwrite(&pixelArr[pixelOffset], 1, paddedRowSize, outputFile);
+		fwrite(&(tmpPixelArr[pixelOffset]), 1, paddedRowSize, outputFile);
 	}
 	fclose(outputFile);
 
 	return;
+}
+
+
+string utils::ImageFile::GetFileName(string filePath)
+{
+	vector<string> v = utils::Func::ExplodeString(filePath, '/');
+	return v[v.size() - 1];
+}
+
+
+/**
+ * Written by Eric Scrivner
+ **/
+vector<string> utils::Func::ExplodeString(string str, char del)
+{
+	string next;
+	vector<string> result;
+
+	// For each character in the string
+	for (string::const_iterator it = str.begin(); it != str.end(); it++) {
+		// If we've hit the terminal character
+		if (*it == del) {
+			// If we have some characters accumulated
+			if (!next.empty()) {
+				// Add them to the result vector
+				result.push_back(next);
+				next.clear();
+			}
+		}
+		else {
+			// Accumulate the next character into the sequence
+			next += *it;
+		}
+	}
+	if (!next.empty())
+		result.push_back(next);
+	return result;
 }
--- NLM/Utils.h
+++ NLM/Utils.h
@@ -9,8 +9,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 
 
+/*************************************
+ *      Values for Bitmap I/O
+ *************************************/
 #define DATA_OFFSET_OFFSET 0x000A
 #define WIDTH_OFFSET 0x0012
 #define HEIGHT_OFFSET 0x0016
@@ -20,6 +24,10 @@
 #define NO_COMPRESION 0
 #define MAX_NUMBER_OF_COLORS 0
 #define ALL_COLORS_REQUIRED 0
+#define ONLY_8_BIT_BMP
+#define ONLY_RECTANGULAR
+#define NORMALIZE_PIXELS
+#define FIX_PIXELS_OUT_OF_BOUND
 
 typedef unsigned int int32;
 typedef short int16;
@@ -32,30 +40,81 @@ using namespace std;
 namespace utils
 {
 
+	/**************************
+	 *      Program Timer
+	 **************************/
 	class Clock
 	{
 	private:
-		clock_t clockT;
+
+		clock_t clockT;		// Timer
 
 	public:
 		Clock();
+
+		/**
+		 * Record starting time
+		 **/
 		void startClock();
+
+		/**
+		 * Record Stop time and return the value in seconds
+		 **/
 		string stopClock();
+
+		/**
+		 * Pause Thread for milliseconds
+		 **/
 		void sleep(int ms);
 	};
 
 
+	/**************************
+	 *  File / Bitmap Manager
+	 **************************/
 	class ImageFile
 	{
+	private:
+		int mSize;			// Malloc Size
+
 	public:
+		
+		float* pixelArr;	// Array of pixel values
+		int height;			// Pixel Height
+		int width;			// Pixel Width
+		int bytesPerPixel;	// Bytes to use per pixel (when writing)
+
 		ImageFile();
-		byte* pixelArr;
-		int height;
-		int width;
-		int bytesPerPixel;
+
+		/**
+		 * Read BMP image file into 'pixelArr'.
+		 **/
 		void Read(string filePath);
+
+		/**
+		 * Write BMP image from 'pixelArr' into the file system.
+		 **/
 		void Write(string filePath);
 
+		/**
+		 * Get Filename part of path
+		 **/
+		static string GetFileName(string filePath);
+
+	};
+
+
+	/**************************
+	 *      Miscellaneous
+	 **************************/
+	static class Func
+	{
+	public:
+
+		/**
+		 * Break String into a vector, by a delimiter
+		 **/
+		static vector<string> ExplodeString(string str, char del);
 	};
 
 }
