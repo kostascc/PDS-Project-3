ca789d713e4b4514901eb7effce11012f39e045e
Kostas <kachatzis@ece.auth.gr>
2021-02-26 20:23:53 +0200

    Add temporary Cuda Implementation

--- NLM/GPU.cu
+++ NLM/GPU.cu
@@ -1,51 +1,245 @@
-/**
+﻿/**
  * (C) 2021 Konstantinos Chatzis
  * Aristotle University of Thessaloniki
  **/
 
 #include "GPU.cuh"
 
+
 using namespace std;
 
-using std::cout;
-using std::generate;
-using std::vector;
 
-// Threads per CTA dimension
-constexpr int _THREADS = 1 << 5;
-
-// Size of shared memory per Thread Block
-constexpr int _SHMEM_SIZE = _THREADS * _THREADS;
-
-
-
-
-int main2(int argc, char** argv)
+namespace GPU
 {
 
-	int _n = 0;
-	int _threads = _THREADS;
-	int _blocks = (_n + _threads - 1) / _threads;
 
-	dim3 _dim_blocks(_blocks, _blocks);
-	dim3 _dim_threads(_threads, _threads);
-
-	krnl <<<_dim_blocks, _dim_threads >>> ();
-	cudaDeviceSynchronize();
+	int run(Parameters params)
+	{
 
 
+#ifdef DEBUG
+		cout << "GPU Starting \n";
+#endif
 
-	return EXIT_SUCCESS;
+		// Start Clock
+		utils::Clock clock = utils::Clock();
+		clock.startClock();
+
+		utils::ImageFile img = utils::ImageFile();
+		img.Read(params.input.imgPath);
+
+#ifdef DEBUG
+		cout << "Image: " << img.width << "x" << img.width << " (Pixels: " << pow(img.width, 2) << ")\n";
+#endif
+
+#ifdef USE_LOG_FILE
+		ofstream log;
+		log.open("./livelog_GPU.txt");
+#endif
+
+		// TODO: nvcc/linker goes balls with this line for some reason..
+		//float sigmaSquared = (float)pow(params.algorithm.sigma, 2);
+		float sigmaSquared = params.algorithm.sigma * params.algorithm.sigma;
+
+
+		/*********************************************
+		 *   Pixels (Noisy / Resulting)
+		 *
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~  ← [img_height]
+		 *                 ↑
+		 *              [img_width]
+		 *********************************************/
+
+		 // Device' Noisy Pixels
+		float* pixN_d;
+		cudaMalloc(&pixN_d, img.width * img.width * sizeof(float));
+		// Copy Pixels from Image File
+		cudaMemcpy(pixN_d, img.pixelArr, img.width * img.width * sizeof(float), cudaMemcpyHostToDevice);
+
+
+		// Device' resulting Pixels
+		float* pix_d;
+		cudaMalloc(&pix_d, img.width * img.width * sizeof(float));
+		//cudaMallocManaged(&pix_d, sizeof(Parameters)); // not managed, so it stays between kernel calls
+		// Set Pixels to 0.0f
+		cudaMemset(pix_d, 0.0f, img.width * img.width * sizeof(float));
+
+
+
+		/*********************************************
+		 *   Weight Sums
+		 *
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~
+		 *   ~ ~ ~ ~ ~ ~ ~ ~  ← [img_height - patch_size]
+		 *                 ↑
+		 *         [img_width - patch_size]
+		 *********************************************/
+
+
+		 // Device's Weight Sums
+		 // (img_width-patch_size) * (img_width-patch_size)
+		float* wSum_d;
+		cudaMalloc(&wSum_d, (img.width - params.algorithm.patchSize) * (img.width - params.algorithm.patchSize) * sizeof(float));
+		// Copy Pixels from Image File
+		cudaMemset(wSum_d, 0.0f, (img.width - params.algorithm.patchSize) * (img.width - params.algorithm.patchSize) * sizeof(float));
+
+
+		/*************************************************
+		 *   Shared Memory (per ThreadBlock)
+		 *
+		 *    +---+---+---+---+---+-------+---+---+
+		 *    | o | o | o | o | o | . . . | o | o |
+		 *    +---+---+---+---+---+-------+---+---+
+		 *      ⥎   ⬑--------------------------⬏
+		 *     wSum        pixN_d (2D patch)
+		 *
+		 *   [ 1 + (patch_size * patch_size) ] * <float>
+		 *************************************************/
+		 // Shared memory (Bytes)
+		int sharedBytes = (1 + pow(params.algorithm.patchSize, 2)) * sizeof(float);
+
+
+		/*************************************************
+		 *   Blocks
+		 *   +---+---+---+---+
+		 *   | B |   |   |   |
+		 *   +---------------+
+		 *   |   |   |   |   |
+		 *   +---------------+
+		 *   |   |   |   |   |
+		 *   +---------------+
+		 *   |   |   |   |   |
+		 *   +---+---+---+---+  ← [img_height]
+		 *                   ↑
+		 *              [img_width]
+		 * 
+		 * 
+		 *   Threads (for each block)
+		 *   +---+---+---+
+		 *   | T |   |   |  
+		 *   +-----------+
+		 *   |   |   |   | 
+		 *   +-----------+
+		 *   |   |   |   |
+		 *   +-----------+  ← [THREADS_Y]
+		 *               ↑
+		 *          [THREADS_X]
+		 * 
+		 * 
+		 * 
+		 *  Patches (for each Thread)
+		 * 
+		 *   +---+---+---+---+
+		 *   | a | a | b | b |
+		 *   +---------------+
+		 *   | a | a | b | b |
+		 *   +-----------+---+
+		 *   | c | c |       |
+		 *   +-------+  ...  |
+		 *   | c | c |       |
+		 *   +---+---+-------+  ← [img_height / THREADS_Y]
+		 *                   ↑
+		 *          [img_width / THREADS_X]
+		 *
+		 *   All 'a' patches will be executed by the same thread.
+		 *   The same for 'b' patches etc.
+		 * 
+		 *
+		 *************************************************/
+		dim3 threads(THREADS_X, THREADS_Y);
+		dim3 blocks(img.width, img.height);
+
+#ifdef DEBUG
+		cout << "Weight Sum Kernel:\n"
+			<< "> Blocks / Threads / Patches: "
+			<< "[" << blocks.x << "," << blocks.y << "] / "
+			<< "[" << threads.x << "," << threads.y << "] / "
+			<< pow(img.width - params.algorithm.patchSize, 2) << "\n"
+			<< "> Shared Memory Per ThreadBlock: " << sharedBytes << " B\n";
+#endif
+
+
+
+		kernelWeightSum __KERNEL3(blocks, threads, sharedBytes) (wSum_d, pixN_d, img.width, params.algorithm.patchSize, sigmaSquared);
+		cudaDeviceSynchronize();
+
+		float* wSum = (float*)malloc((img.width - params.algorithm.patchSize) * (img.width - params.algorithm.patchSize) * sizeof(float));
+		cudaMemcpy(wSum, wSum_d, (img.width - params.algorithm.patchSize) * (img.width - params.algorithm.patchSize) * sizeof(float), cudaMemcpyDeviceToHost);
+
+		for (int i = 0; i < img.width - params.algorithm.patchSize; i++)
+		{
+			for (int j = 0; j < img.width - params.algorithm.patchSize; j++)
+			{
+				log << wSum[i * (img.width - params.algorithm.patchSize) + j] << " ";
+			}
+			log << "\n";
+		}
+
+		log << "\n\n\n";
+
+		// Copy Resulting Pixels into hosts' image matrix
+		cudaMemcpy(img.pixelArr, pix_d, img.width * img.width * sizeof(float), cudaMemcpyDeviceToHost);
+
+		cudaFree(pixN_d);
+		cudaFree(wSum_d);
+		cudaFree(pix_d);
+
+		// Save Image
+		img.Write(params.input.outputDir + "/sigma" + to_string(params.algorithm.sigma) + "_" + utils::ImageFile::GetFileName(params.input.imgPath));
+
+
+		cout << "GPU Took " << clock.stopClock() << "\n";
+
+
+
+#ifdef USE_LOG_FILE
+
+		log << "\n\n";
+
+		for (int i = 0; i < 16; i++)
+		{
+			for (int j = 0; j < 16; j++)
+			{
+				log << pix[i * 16 + j] << " ";
+			}
+			log << "\n";
+		}
+
+		log.close();
+#endif
+
+		return 0;
+	}
+
+
+
+	__global__ void kernelWeightSum(float* wSum_d, float* pixN_d, int imgWidth, int patchSize, float sigmaSquared)
+	{
+
+		// Shared Memory
+		extern __shared__ float _shmem[];
+
+		return;
+	}
+
+
+	int iDivUp(int a, int b)
+	{
+		return ((a % b) != 0) ? (a / b + 1) : (a / b);
+	}
+
+
+
 
 }
 
 
-
-__global__ void krnl()
-{
-	int row = threadIdx.x * blockDim.x + threadIdx.x;
-	int col = threadIdx.y * blockDim.y + threadIdx.y;
-
-
-	return;
-}
--- NLM/GPU.cuh
+++ NLM/GPU.cuh
@@ -6,30 +6,51 @@
 #ifndef GPU_CUH
 #define GPU_CUH
 
-
-#include <algorithm>
-#include <cassert>
-#include <cstdlib>
-#include <functional>
-#include <iostream>
-#include <vector>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include "device_launch_parameters.h"
 #include <stdlib.h>
-#include <assert.h>
-#include <iostream>
-#include "Utils.h"
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include "math.h"
+#include "vector_functions.h"
+#include "Utils.h"
+#include "Parameters.h"
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include <math.h>
+#include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
+#include "device_functions.h"
+#include "device_launch_parameters.h"
+#include "cuda_device_runtime_api.h"
+//#include "cuda/atomic"
+//#include "cuda/std/atomic"
 
 
-#include <omp.h>
 
 
+#define DEBUG
 #define USE_LOG_FILE
+
+
+// Block Dimensions: 8x8 threads
+//   = 64 threads
+//   = 2 warps
+#define THREADS_X 8
+#define THREADS_Y 8
+
+
+// nvcc errors from intellisense workaround.
+#ifdef __INTELLISENSE__
+#define __KERNEL2(grid, block)
+#define __KERNEL3(grid, block, sh_mem)
+#define __KERNEL4(grid, block, sh_mem, stream)
+#define __syncthreads();
+#define atomicAdd(a,b);
+
+#else
+#define __KERNEL2(grid, block) <<< grid, block >>>
+#define __KERNEL3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
+#define __KERNEL4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
+#endif
 
 
 using namespace std;
@@ -39,6 +60,12 @@ namespace GPU
 {
 
 	int run(Parameters params);
+
+	__global__ void kernelWeightSum(float* wSum_d, float* pixN_d, int imgWidth, int patchSize, float sigmaSquared);
+
+	__global__ void kernelPatchPixels(float* wSum_d, float* pixN_d, int imgWidth, int patchSize, float sigmaSquared);
+
+	int iDivUp(int a, int b);
 
 }
 
